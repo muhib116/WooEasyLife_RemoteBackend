147eb912b0446c6b5251bba1d373993544569640
Nasrullah(nasir) <nasrullah23a@gmail.com>
2024-12-07 06:27:06 +0600

    hit track ongoing

---
+++ database/migrations/2024_12_07_000824_create_route_hits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('route_hits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('route_name');
+            $table->integer('hit_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    //  Route could be group. that means a group of route will be included that hit counts.
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('route_hits');
+    }
+};
