c901b6eee4dc727f317c890ecbc7215200141132
Nasrullah(nasir) <nasrullah23a@gmail.com>
2025-01-17 18:18:05 +0600

    Move get-metadata route outside of auth middleware for public access

--- routes/api.php
+++ routes/api.php
@@ -36,9 +36,9 @@ Route::middleware('auth:sanctum')->get('/validate-token', function (Request $req
 Route::get('app-logo', [PluginsController::class, 'appLogo']);
 
 Route::get('download-plugins', [PluginsController::class, 'downloadApp']);
+Route::get('get-metadata', [PluginsController::class, 'getMetadata']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('get-metadata', [PluginsController::class, 'getMetadata']);
 
     Route::group(['as' => 'courier.', 'prefix' => 'courier'], function () {
         Route::post('/list', [ConfigurationController::class, 'getList']);
