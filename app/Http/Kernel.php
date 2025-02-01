528ed57b5a6e3bb0a198989fb99a37cd34561501
Nasrullah(nasir) <nasrullah23a@gmail.com>
2025-02-02 04:56:42 +0600

    Add custom authentication middleware to HTTP kernel for token validation

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check.token' => \App\Http\Middleware\CustomAuthMiddleware::class,
         'check.tokenDomain' => \App\Http\Middleware\ValidateTokenDomain::class,
     ];
 }
