592c212eaa4cd6b7e5357e52f1af4275c97a651c
Nasrullah(nasir) <nasrullah23a@gmail.com>
2025-04-17 00:51:20 +0600

    Refactor User Analysis report to display product sales with links and quantities

--- resources/js/Pages/UserAnalysis/Index.vue
+++ resources/js/Pages/UserAnalysis/Index.vue
@@ -25,7 +25,22 @@
             </template>
 
             <template #content>
-                <div v-for="item in uniqueLinksItems || []">
+                <div
+                    v-for="item in product_sale || []"
+                    class="mb-4 rounded-lg border px-4 py-2"
+                >
+                    <a
+                        :href="item?.item?.product_url"
+                        class="hover:text-blue-500"
+                        target="blank"
+                    >
+                        {{ item?.item?.name }}
+                    </a>
+                    <div>
+                        {{ item?.total_quantity }}
+                    </div>
+                </div>
+                <!-- <div v-for="item in uniqueLinksItems || []">
                     <template v-if="item?.products">
                         <a
                             v-for="product in item?.products || []"
@@ -42,7 +57,7 @@
                         target="_blank"
                         >{{ item?.name }}</a
                     >
-                </div>
+                </div> -->
             </template>
         </Card>
     </AuthenticatedLayout>
@@ -63,6 +78,7 @@ const selectedUserId = ref();
 const report = ref([]);
 const uniqueLinks = ref([]);
 const uniqueLinksItems = ref([]);
+const product_sale = ref({});
 
 const fetchReport = async () => {
     if (!selectedUserId.value) {
@@ -81,6 +97,7 @@ const fetchReport = async () => {
 
 const getUniqueLinks = (data) => {
     uniqueLinks.value = [];
+    product_sale.value = {};
     const useDetails = data.flatMap((item) => item.use_details);
     // console.log(useDetails)
     each(useDetails, (item) => {
@@ -89,7 +106,6 @@ const getUniqueLinks = (data) => {
         }
         if (isArray(item?.cart_contents)) {
             each(item?.cart_contents, (content) => {
-                // console.log(content);
                 if (!uniqueLinks.value.includes(content?.product_url)) {
                     uniqueLinks.value.push(content?.product_url);
                     uniqueLinksItems.value.push(content);
@@ -101,7 +117,35 @@ const getUniqueLinks = (data) => {
                 uniqueLinksItems.value.push(item?.cart_contents);
             }
         }
+
+        // =======================================
+        const contents = item.cart_contents;
+
+        const items = Array.isArray(contents) ? contents : [contents];
+
+        items.forEach((item) => {
+            const url = item.product_url;
+
+            if (!product_sale.value[url]) {
+                product_sale.value[url] = {
+                    value: [],
+                    item,
+                    total_quantity: 0,
+                };
+            }
+
+            const quantity = parseInt(item.quantity);
+
+            product_sale.value[url].value.push({
+                quantity: quantity,
+                item: item,
+                product_url: url,
+            });
+
+            product_sale.value[url].total_quantity += quantity;
+        });
     });
-    console.log(uniqueLinks.value);
+    // console.log(uniqueLinks.value);
+    // console.log(product_sale.value);
 };
 </script>
