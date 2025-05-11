d3169ed650c9cc82fcec9a8a6d9f4f9cd079c7b8
Nasrullah(nasir) <nasrullah23a@gmail.com>
2025-05-11 08:17:27 +0600

    update steadfast fraud check things

--- app/Http/Controllers/FraudCheckController.php
+++ app/Http/Controllers/FraudCheckController.php
@@ -270,16 +270,16 @@ class FraudCheckController extends Controller
                 CURLOPT_HTTPHEADER => [
                     "accept: application/json, text/plain, */*",
                     "accept-language: en-US,en;q=0.9",
-                    "cookie: remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Inh0VFpKTXFYSzJ6VEJ5d0RCVHE1b2c9PSIsInZhbHVlIjoiTkFKaGNSM1ZvTit0VmYxa01LRXZKcjFqc1QxSTNkckxqUk0ya1Y3aEUvUnVFMFhyUHE2WkFjLzV5UGtRd1ptS1dGa3FxeVNiOHROSHFLQXBMQytkcW16eUxYeWVhZ3llL3YzM2szOWIxTnJBcURMclRmc1NDVWNhdzhXa1B0WDVMK0tFakJqVmplK29OMUlrYW9QeXJMYWtSVlpKbXRaVnAvN05HOHpTb1VYWGJQdi93RTVxOGZnZjBvb0hmZWFjYk0zb09QSEUxbFRSSGxjZXJialNKbS9iZmdGbVFOZ0FrMVFhVC9DcitJaz0iLCJtYWMiOiI0OWMxYTgzNjAyMjcxMGUxOGY3NDg3N2ZlYTYyZDE2NDVmZDQyYmJiMmI0YTI1NTcxMWYyNTk4YWNjNzJiMmI3IiwidGFnIjoiIn0%3D; cf_clearance=qYEGknO8p1SD7pc39hAEPdqyWQU.uzgVyiz_BJ3f2rg-1732108103-1.2.1.1-i30GzOXCwmAncFgAcRF9LJFGW4.8JCpy5_pUNU4PSqA_ioIsRB_4CCoM6MozhmjKU7.iPYDZN2dQH2YJRGo08zllyFLn3T6ttt81xXEu4O8Ze36hoDsZn.KU4nXK02RV1AFEjZSE0n2wHSxJIEOrhRsZ5UibWj.KI_Vo1bEBiC.H7fAF.2Ia4.KB.HnPHbzBnw0e4DLnEU60R0MTge8wLrXGotCG5QroWgi4sUzRO1et74SyGIbniK6Rqc6jtX7jHE8PMTtG5ZRaaT39MEgLUHKY_A00uWkm5g9VWKvZF3mkz10cG1EgkmgUvtBT.guGQ7vvcQyDwLqhQqpTgSTZI.eT3JhzLDoZaNWQC0KOufrZaqmQXQYtqE7Y9LMgTBolb32PVcORvRdHEFddntBpOw; XSRF-TOKEN=eyJpdiI6IjZpTUNKVkgrNkwrdVVlUExySjFRN1E9PSIsInZhbHVlIjoiYmlPVm9qVjFqb21JcFlMTjJPSDVSZ2d6Y0ZqSG12allrN1c3TlRhOHZSSjNhbXJVNiszV1JSWGFkUEVueUhGVUg2RzJybSszZU5xM2JSMzE1enpZVjR6UTZvSDAwSHgyWTZzWElybGdpdGV6L21zOVZ0MzBwblVDUklHU0VWYkoiLCJtYWMiOiI0NTNjM2M3MTI2MWJkYjZlNjM1ZDkzOThhYWFmN2Y1MTg3ODQxN2VlZGFlODAwOGViMzQ3Mjk2YjRlNmJkNDIzIiwidGFnIjoiIn0%3D; steadfast_merchant_session=eyJpdiI6Ik5zcHJLSXphcGFUVEl0TlZiYVA5Wnc9PSIsInZhbHVlIjoic0wvRzBnbmdKbEdIeUJwZ1VySkRiYmlRSFVqNDY2aXM2WHJvQkpIaTA0ZWZrTnA2RnZMbmQ2bmdiYnpGcUdzVzUzZUpaYjlWZSs0YWVFbi95N1BuTGtoVjhWd01pNW8vRkN1WjUxRnpsR1ZjeVpEZ1M3QkR3bUJKVEFTUWhSeXEiLCJtYWMiOiJkZTBjMDk4YjU1NTFmODg3NDA2MTI5OThjMWE0ZWEwMjI3NjFiZmE5YTFjOTMzMWQzY2FjM2ViZDUwNjgzMzI2IiwidGFnIjoiIn0%3D",
+                    "cookie: remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ii9FRDAwRzZkSmlYL3o0OFBSSUtDWGc9PSIsInZhbHVlIjoiamt4VE9iRlpYc0NHcWN1bXM3TDlMTzFDT2o4bG01a3M3eFhHSVhzY0xIVGNSQVRYRmtieDBRVzE2WlZPU3dIb3ErSzAvRHgvZkRzWkxpUWZnY21iQUh4aVhtcGVIaU9kVWUyaGtYMm5hdWlYdmdlOVhHRGw0RTNZcDJ5RDBJLzFBOC82NUpaWkU4NS93cks1d0hkcXpEZFZIK2Zyam9URFlkNDQxcW9jSmZNMFlnV0s4TUNOODROdm1NckUwQWdvd1BiMitiS3lmbTl4VkFzT1E5R055MUxEOEE2QUNPdGZFUzU1MDNwakJmMD0iLCJtYWMiOiIxYmY5YjExZjIwNjA4NzRkY2Q4OTg1MWZhYWNjZDQ3YWEwYzU5MzI4MjU2YzUzMTdmMzY2Mjk3NDk5YTE2ZjRlIiwidGFnIjoiIn0%3D; XSRF-TOKEN=" . urlencode($xsrfToken) . "; steadfast_merchant_session=eyJpdiI6ImkvWnVvcVZmMXNUZ21aVU9LbFA5aFE9PSIsInZhbHVlIjoibFpJRFo3WHQzcjZZNis2NjNwY0JZZ1ludWxsQ2g0cjI5N2pmeGdyWWJvUUxRRmRGeEVhTEtMNmZHdUg0dFYzQk56MWQ4TTF0UWIrTGtpY3lkZVpIaUhrTktwL0hlQmxUaG1CU2ZGTkFXR2p0WExlZDMzV2VpQW9oeDE1bytNYUwiLCJtYWMiOiI4OTE3ZmJkZDNjMzNkZTJlNWQxYjZiNWJiZmUyYWE3NDk5YmExNTkyMzE0NGI3MWY4MjBjY2JjMmQ3YmQ1NzA1IiwidGFnIjoiIn0%3D",
                     "priority: u=1, i",
                     "referer: https://steadfast.com.bd/user/frauds/check",
-                    "sec-ch-ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+                    "sec-ch-ua: \"Chromium\";v=\"136\", \"Google Chrome\";v=\"136\", \"Not.A/Brand\";v=\"99\"",
                     "sec-ch-ua-mobile: ?0",
                     "sec-ch-ua-platform: \"macOS\"",
                     "sec-fetch-dest: empty",
                     "sec-fetch-mode: cors",
                     "sec-fetch-site: same-origin",
-                    "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+                    "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
                     "x-requested-with: XMLHttpRequest",
                     "x-xsrf-token: " . $xsrfToken
                 ],
@@ -289,7 +289,7 @@ class FraudCheckController extends Controller
             $err = curl_error($curl);
 
             curl_close($curl);
-
+            LogHelper::saveLog('hi', $curl_response);
             $response = json_decode($curl_response);
             $confirm_order = @$response[0];
             $cancel_order = $response[1];
