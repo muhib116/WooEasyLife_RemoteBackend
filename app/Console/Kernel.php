bf9d3ad5858791aae5dbb354699c83c7719d78e3
Nasrullah(nasir) <nasrullah23a@gmail.com>
2025-10-08 03:33:37 +0600

    schedule update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('test:scheduler')->daily();
+        $schedule->command('test:scheduler')
+            ->dailyAt('00:30')
+            ->timezone('Asia/Dhaka');
     }
 
     /**
